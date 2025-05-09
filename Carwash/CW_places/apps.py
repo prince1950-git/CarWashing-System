53d5f18e819aade598b4093ee202deea20dafaf6
prince1950-git <e1062240002@TIMSCDRMUMBAI.IN>
2025-05-09 11:23:48 +0530

    Update apps.py

--- Carwash/CW_places/apps.py
+++ Carwash/CW_places/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+print("My name is Priyanshu")
 class CarwashPlacesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CW_places'
